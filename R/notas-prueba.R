dede998614b04db3f224cb370b5d232142faada2
raulmejia <r8a8m8p8@gmail.com>
2021-08-09 21:06:05 +0200

    Initial commit

--- R/notas-prueba.R
+++ R/notas-prueba.R
@@ -1,5 +1,28 @@
+#  install.packages("gh")
+#  install.packages("gert")
+#  install.packages("gitcreds")
+install.packages("usethis")
+
+
+library("gh")
+library("gert")
+library("gitcreds")
+library( "usethis")
+
+#.libPaths()
+
+usethis::use_r("notas-prueba.R")
+
 gitcreds::gitcreds_set()
 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
 usethis::use_git()
 usethis::edit_git_config()
 usethis::use_github()
+
+
+usethis::create_project("Su")
+
+usethis::create_project("Su_Usuario.github.io")
+setwd()
+getwd()
+usethis::edit_r_environ()
